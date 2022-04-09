821a497a3c0befad90c7f8291637f513d2347321
Timothy Burroughs <90850361+tim-burr@users.noreply.github.com>
2022-04-09 14:55:43 -0400

    Remove extra files not needed for publication
    Keep all files in main (source) branch

--- scripts/archive/shared.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function loadSharedElements() {
-    // Navigation Bar
-    $('header').load('/elements/nav.html');
-    // Footer
-    $('footer').load('/elements/footer.html');
-})
