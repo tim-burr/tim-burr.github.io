a9ecab314777cbd23078dc0124ae07b386dcf3e8
Timothy Burroughs <president@timbur.tech>
2022-10-15 12:00:06 -0400

    Add client-side nav & footer generation
    Javascript-based.

--- scripts/menu-size.js
+++ scripts/menu-size.js
@@ -1,12 +1,14 @@
-/* Toggle responsive styling */
-window.addEventListener('resize', function isOverflown() {
+/* Event Subscription: Check menu format everytime window is resized */
+window.addEventListener('resize', isOverflown, false)
+
+/* Toggle responsive styling as necessary */
+function isOverflown() {
     var menu = document.getElementById("nav-menu");
     var overflown = menu.scrollHeight > menu.clientHeight || menu.scrollWidth > menu.clientWidth;
     
-    // If default className only, apply responsive class
     overflown?
         menu.classList.add("nav-responsive"):
         menu.classList.remove("nav-responsive");
-    
+
     return overflown;
-})
+}
