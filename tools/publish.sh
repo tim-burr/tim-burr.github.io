c9e440214fca24fcc863e3ed4757f73f5af43b2a
Timothy Burroughs <president@timbur.tech>
2022-10-15 12:00:12 -0400

    Disable GitHub Workflow
    Disabled custom workflow online, but leaving updated publish.yml in the repo for future integration.
    
    Removed and untracked .vscode directory from the repo.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+# PATHS
+curr_dir = 
 source_dir = ../
 publish_dir = ../_site
+
+# Git Branches
 source_branch = main
 publish_branch = published
 
-build_command() {
+# Commands
+build() {
   build/build-site.py
 }
 
-#echo -e "\033[0;32mDeleting old content...\033[0m"
-#rm -rf $publish_dir
-
+# Script
 echo -e "\033[0;32mChecking out $publish_branch....\033[0m"
 git worktree add $publish_dir $publish_branch
 
 echo -e "\033[0;32mGenerating site...\033[0m"
-build_command
+build
 
 echo -e "\033[0;32mDeploying site to $publish_branch branch...\033[0m"
 cd $publish_dir &&
@@ -24,5 +27,7 @@ cd $publish_dir &&
   git commit -m "Deploy updates" &&
   git push origin $publish_branch
 
-#echo -e "\033[0;32mCleaning up...\033[0m"
-#git worktree remove $publish_dir
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove $publish_dir
+
+cmd /k #(optional)
