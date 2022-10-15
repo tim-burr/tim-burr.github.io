659f8aeabdd2ebf48ff6f67298267f89ddc86990
Timothy Burroughs <president@timbur.tech>
2022-10-15 12:00:15 -0400

    Add robots and sitemap
    Consolidated robots, sitemap, and CNAME in source _data directory.

--- tools/build/build_site.py
+++ tools/build/build_site.py
@@ -8,10 +8,11 @@ from utils.config_loader import *
 from utils.generator import *
 
 # Directories
+CURR_DIR = Path(__file__).parent.resolve()
 ROOT_DIR = Path(__file__).parents[2].resolve()
+
 # Files
-DEF_CONFIG = ROOT_DIR / "_data/config.yml" # Default
-CNAME = ROOT_DIR / "CNAME"
+DEF_CONFIG = CURR_DIR / "config.yml" # Default
 
 ########################
 # Main
@@ -39,7 +40,9 @@ if __name__=="__main__":
     # Create build directory
     create_directory(build_dir)
     # Establish custom domain
-    copy_file(CNAME, build_dir)
+    copy_file(ROOT_DIR / "_data/CNAME", build_dir) # TODO: Loop on copying everything in _data dir
+    copy_file(ROOT_DIR / "_data/sitemap.xml", build_dir)
+    copy_file(ROOT_DIR / "_data/robots.txt", build_dir)
     # Build page styles
     copy_directory(style_dir, build_dir) # TODO: Make below copy_dir calls extensible based on config assets (loop?)
     # Build media
